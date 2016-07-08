859f43c4d9be80428b0b560049f45cdb9c9eef7b
Andre Hahn <andre.hahn@tfgco.com>
2016-07-08 18:12:20 -0300

    Sending image to docker

---
+++ push_to_docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+VERSION=$(cat ./api/version.go | grep "var VERSION" | awk ' { print $4 } ' | sed s/\"//g)
+
+docker build -t arkadiko .
+docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker tag arkadiko:latest tfgco/arkadiko:$VERSION.$TRAVIS_BUILD_NUMBER
+docker push tfgco/arkadiko:$VERSION.$TRAVIS_BUILD_NUMBER
+
+DOCKERHUB_LATEST=$(python get_latest_tag.py)
+
+if [ "$DOCKERHUB_LATEST" != "$VERSION.$TRAVIS_BUILD_NUMBER" ]; then
+  echo "Last version is not in docker hub!"
+  echo "docker hub: $DOCKERHUB_LATEST, expected: $VERSION.$TRAVIS_BUILD_NUMBER"
+  exit 1
+fi
