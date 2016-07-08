1500683912a2e4e664ac3bd85d678084a748d3fe
Andre Hahn <andre.hahn@tfgco.com>
2016-07-08 18:27:58 -0300

    Added version information

---
+++ api/version.go
@@ -0,0 +1,11 @@
+// arkadiko
+// https://github.com/topfreegames/arkadiko
+//
+// Licensed under the MIT license:
+// http://www.opensource.org/licenses/mit-license
+// Copyright © 2016 Top Free Games <backend@tfgco.com>
+
+package api
+
+// VERSION identifies Arkadiko's current version
+var VERSION = "0.1.0"
